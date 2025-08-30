72b556e10f242d88e6042cb9f628ac9c9d061979
Braian-de-Liz <delizbraian@gmail.com>
2025-08-30 08:59:01 -0300

    first commit

---
+++ bd.php
@@ -0,0 +1,16 @@
+<?php 
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "root";
+    $banco = "saep_db";
+
+    $conecao = new mysqli($servidor, $usuario, $senha, $banco);
+    
+    // verificar erros de conexão
+
+    if($conecao -> connect_error){
+        die("Falha na conexão : " . $conecao -> connect_error);
+    }
+
+    $conecao->set_charset("utf8");
+?>
---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login - SAEP</h2>
+    <form action="" method="POST">
+        <label for=""> Usuário</label>
+        <input type="text" name="usuario" required>
+
+        <label for="">Senha</label>
+        <input type="passoword" required>
+
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
+
+
+<?php 
+    
+?>
